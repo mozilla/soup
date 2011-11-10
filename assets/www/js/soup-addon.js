04ed36a1409cd6a801e66500065890c910d31d95
Harald Kirschner <hkirschner@mozilla.com>
2011-11-10 14:47:35 -0800

    - FIX: Split SoupActivity into 2 activities for cleaner task handling  - FIX: Correctly keep application states while switching between launcher and apps  - ADD: Inject phonegap into external apps (quick-n-dirty version)  - ADD: Use phonegap and soup bridge from external app

--- assets/www/js/soup-addon.js
+++ assets/www/js/soup-addon.js
@@ -1,26 +1,45 @@
 (function(context) {
-	
+
+	if(context.$soup)
+		return;
 	context.$soup = true;
 
-  // "use strict";
-  
+	// Fix injected phonegap	
+	if (document.readyState == 'complete') {
+		PhoneGap.onDOMContentLoaded.fire();
+	}
+	
+	document.addEventListener("deviceready", function() {
+		navigator.notification.beep(1);
+	}, false);
+
+
+	var readyInterval = context.setInterval(function() {
+		if (Device.available) {
+			context.clearInterval(readyInterval);
+		}
+	}, 1000);
+
+
+	// "use strict";
+
 	// TODO: .apps bridge
 	// navigator.mozApps = navigator.mozApps || {};
-	
+
 	// .id bridge
 	navigator.id = navigator.id || {};
-	
+
 	navigator.id._getVerifiedEmailOrigin = navigator.id.getVerifiedEmail;
-	
+
 	navigator.id.getVerifiedEmail = function(callback) {
 		console.log('bridged navigator.id.getVerifiedEmail');
-		
+
 		navigator.id._getVerifiedEmailOrigin(function(assertion) {
 			console.log(assertion);
 			callback(assertion);
 		});
 	};
-	
-	console.log('navigator.id bridged ' + location.host);
-	
+
+	console.log('navigator.id bridged ' + (location.host || 'file://'));
+
 })(this);
